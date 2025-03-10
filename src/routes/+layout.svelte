223311087f422d28a6f523a5ac152c2c7dde3c3e
PillowGit <goldentaco727@gmail.com>
2025-03-10 09:57:33 -0700

    Started

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<title>Pillow</title>
+<div class="flex min-h-[80vh] w-screen flex-col items-center justify-center">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { animate } from 'motion';
+
+	onMount(() => {
+		animate(title, { opacity: 1 }, { duration: 1 }).then(() => {
+			animate(sub, { opacity: 1 }, { duration: 1.5 });
+		});
+	});
+	let title: HTMLHeadingElement;
+	let sub: HTMLHeadingElement;
+</script>
+
+<h1 bind:this={title} class="text-5xl font-bold opacity-0">pillow.dev</h1>
+<h2 bind:this={sub} class="mt-8 text-xl opacity-0">rework coming soon</h2>
