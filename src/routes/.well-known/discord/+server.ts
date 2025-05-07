093c12dd08e1dcf5f79f0708dd4ab5fb5e1f529e
PillowGit <goldentaco727@gmail.com>
2025-05-07 10:37:25 -0700

    Remove dumb discord shit

--- src/routes/.well-known/discord/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async ({ url }) => {
-  return json("dh=40be569a3571fe2ec8d2988d51a0b72b12ab52b4");
-}
