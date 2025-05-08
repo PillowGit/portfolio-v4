997dd27c2c4841cd4d156ac0b1a53da7b54a1d14
PillowGit <goldentaco727@gmail.com>
2025-05-07 22:52:13 -0700

    Remove about

--- src/lib/components/Navitems.ts
+++ src/lib/components/Navitems.ts
@@ -6,7 +6,6 @@ interface NavItem {
 }
 const navitems: NavItem[] = [
 	{ name: 'Home', href: '/' },
-	{ name: 'About', href: '/about' },
 	{ name: 'Projects', href: '/projects' },
 	{ name: 'Skills', href: '/skills' },
 	{ name: 'Contact', href: '/contact' }
